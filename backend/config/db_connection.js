10cbca0cdcb0bb7c042d2b1c64c50f40561e5b0c
gampa-anupama <anupamagampa1@gmail.com>
2025-10-12 20:50:39 +0530

    changes in connection file

--- backend/config/db_connection.js
+++ backend/config/db_connection.js
@@ -1,23 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const uri = 'mongodb+srv://anupamagampa:Anu456%40%2A@cluster0.tk7wg.mongodb.net/ProjectData?retryWrites=true&w=majority';
-
-// const connectToDB = async () => {
-//   try {
-//     await mongoose.connect(uri, { 
-//       useNewUrlParser: true, 
-//       useUnifiedTopology: true,
-//       serverSelectionTimeoutMS: 10000, // Adjust as needed
-//       socketTimeoutMS: 45000, // Adjust as needed
-//     });
-//     console.log('MongoDB connected');
-//   } catch (error) {
-//     console.error('MongoDB connection error:', error);
-//     throw error;
-//   }
-// };
-
-// module.exports = connectToDB;
 
 const mongoose = require('mongoose');
 require('dotenv').config();  // Load environment variables
